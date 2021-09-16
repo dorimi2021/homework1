466e6625a67a990f7656d5e31254c95856e57b3e
dorimi2021 <dreamy86@gmail.com>
2021-09-16 21:52:54 +0300

    homework 2

---
+++ homework1/homework2.py
@@ -0,0 +1,24 @@
+#  BEGIN  Task 1
+print(8 / 2 + 5 - -3 / 2)
+#  END
+
+#  BEGIN  Task 2 "Khal Drogo's favorite word is "athjahakar""
+print('\"Khal Drogo\'s favorite word is \"athjahakar\"\"')
+#  END
+
+#  BEGIN  Task 3
+print("Winter" + " came" + " for" + " the" + " House" + " of" + " Frey")
+#  END
+
+#  BEGIN  Task 4
+word = "First text"
+print(word)
+word = "Second text"
+print(word)
+#  END
+
+#  BEGIN  Task 5
+print("What's your name?")
+name = input()
+print("Hello, " + name + "!")
+#  END
