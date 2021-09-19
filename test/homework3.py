8d7106f99f49678a4a10fd44d311eee0ee5cc2c0
dorimi2021 <dreamy86@gmail.com>
2021-09-20 01:24:30 +0300

    hm

---
+++ test/homework3.py
@@ -0,0 +1,47 @@
+# #  BEGIN Task 1
+# print("Insert °C")
+# cel = float(input())
+# far = cel * 9 / 5 + 32
+# print(far)
+# #  END
+#
+# #  BEGIN Task 2
+# print("Insert number")
+# number = int(input())
+# if number >= 0:
+#     print(number)
+# else:
+#     print(number * -1)
+# #  END
+#
+# #  BEGIN Task 2 - 2
+# print("Insert number")
+# print(abs(int(input())))
+# #  END
+#
+# # BEGIN Task 3
+# x=int(input())
+# y=int(input())
+# z=int(input())
+# if ((x+y+z)<=1):
+#     print (False)
+# else:
+#     print (True)
+# #  END
+# # BEGIN Task 3 - 2
+# x=int(input())
+# y=int(input())
+# z=int(input())
+# print(x+y+z>1)
+# #  END
+#
+# # BEGIN Task 3 - 3
+# print(int(input())+int(input())+int(input())>1)
+# #  END
+#
+# # BEGIN Task 3 - 4
+sum = 0
+for i in range(0, 3):
+    sum += int(input())
+print(sum>1)
+# #  END
