c29324a1643cdfeac07f595583812e4e95bc881e
dorimi2021 <dreamy86@gmail.com>
2021-09-24 01:35:09 +0300

    hm

--- test/main.py
+++ test/main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+# def count_greater_or_equal(numbers, x):
+#     s=0
+#     for number in numbers:
+#         if (number>=x):
+#             s+=1
+#     return s
+# #print(count_greater_or_equal((1,2,3,4,5), 2))
+#
+# def test_count_greater_or_equal():test_count_greater_or_equal
+#     assert count_greater_or_equal([1, 2, 3, 4, 5], 2) == 4
+#     assert count_greater_or_equal([1, 2, 3, 4, 5], 1) == 5
+#     assert count_greater_or_equal([1, 2, 3, 4, 5], 0) == 5
+#     assert count_greater_or_equal([0, 1], 2) == 0
+#     assert count_greater_or_equal([], 2) == 0
+#     print("Great your solution works!")
+#
+# test_count_greater_or_equal()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def rotate(numbers, rotateIndex):
+    if len(numbers) == 0:
+        return numbers
+    arrayCorect = 1
+    numbersLen = len(numbers) - arrayCorect
+    for outer in range(0, rotateIndex):
+        lastElement = numbers[numbersLen]
+        for inner in range(0, numbersLen):
+            rotatePosition = numbersLen - arrayCorect - inner
+            bigger = numbers[rotatePosition]
+            numbers[rotatePosition + arrayCorect] = bigger
+        numbers[0] = lastElement
+    return numbers
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test_rotate():
+    assert rotate([1, 2, 3], 1) == [3, 1, 2]
+    assert rotate([1, 2, 3], 2) == [2, 3, 1]
+    assert rotate([1, 2, 3], 3) == [1, 2, 3]
+    assert rotate([1, 2, 3], 0) == [1, 2, 3]
+    assert rotate([1], 1) == [1]
+    assert rotate([1], 0) == [1]
+    assert rotate([], 2) == []
+    print("Great your solution works!")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+test_rotate()
