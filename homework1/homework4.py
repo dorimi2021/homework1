29889c649b0cba9998114e561b4f4695ac9213c1
dorimi2021 <dreamy86@gmail.com>
2021-09-24 23:43:17 +0300

    hw 4

--- homework1/homework4.py
+++ homework1/homework4.py
@@ -1,123 +1,109 @@
-#  BEGIN Task 1  Цельсий в Фаренгейт
-print("Insert °C: ")
-cel = float(input())
-far = cel * 9 / 5 + 32
-print(far)
-#  END
+# TASK 1
+def count_greater_or_equal(numbers, x):
+    s = 0
+    for number in numbers:
+        if (number >= x):
+            s += 1
+    return s
 
-#  BEGIN Task 2  Модуль числа
-print("Insert number: ")
-number = int(input())
-if number >= 0:
-    print(number)
-else:
-    print(number * -1)
-#  END
+def test_count_greater_or_equal():
+    assert count_greater_or_equal([1, 2, 3, 4, 5], 2) == 4
+    assert count_greater_or_equal([1, 2, 3, 4, 5], 1) == 5
+    assert count_greater_or_equal([1, 2, 3, 4, 5], 0) == 5
+    assert count_greater_or_equal([0, 1], 2) == 0
+    assert count_greater_or_equal([], 2) == 0
+    print("Great your solution works!")
+test_count_greater_or_equal()
 
-#  BEGIN Task 2 - 2
-print("Insert number: ")
-print(abs(int(input())))
-#  END
+# TASK 2
+def rotate(numbers, n):
+    return numbers[-n:] + numbers[:-n]
 
-# BEGIN Task 3  Голосование
-print("Insert 1 or 0: ")
-x=int(input())
-y=int(input())
-z=int(input())
-if ((x+y+z)<=1):
-    print (False)
-else:
-    print (True)
-#  END
-# BEGIN Task 3 - 2
-print("Insert 1 or 0: ")
-x=int(input())
-y=int(input())
-z=int(input())
-print(x+y+z>1)
-#  END
+def test_rotate():
+    assert rotate([1, 2, 3], 1) == [3, 1, 2]
+    assert rotate([1, 2, 3], 2) == [2, 3, 1]
+    assert rotate([1, 2, 3], 3) == [1, 2, 3]
+    assert rotate([1, 2, 3], 0) == [1, 2, 3]
+    assert rotate([1], 1) == [1]
+    assert rotate([1], 0) == [1]
+    assert rotate([], 2) == []
+    print("Great your solution works!")
+test_rotate()
 
-# BEGIN Task 3 - 3
-print("Insert 1 or 0: ")
-print(int(input())+int(input())+int(input())>1)
-#  END
+# TASK 3
+def extend(a, b):
+    return a + b
 
-# BEGIN Task 3 - 4
-print("Insert 1 or 0: ")
-sum = 0
-for i in range(0, 3):
-    sum += int(input())
-print(sum>1)
-#  END
+def test_extend():
+    assert extend([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert extend([4, 5, 6], [1, 2, 3], ) == [4, 5, 6, 1, 2, 3]
+    assert extend([], [4, 5, 6]) == [4, 5, 6]
+    assert extend([1, 2, 3], []) == [1, 2, 3]
+    assert extend([1], [4]) == [1, 4]
+    print("Great your solution works!")
+test_extend()
 
-# BEGIN Task 4  Факториал
-print("Insert number: ")
-n = int(input())
-fact = 1
-for number in range(1, n + 1):
-    fact = fact * number
-print(fact)
-#  END
+# TASK 4
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+    num = 2
+    while n % num != 0:
+        num += 1
+    return num == n
 
-# BEGIN Task 5  Произведение четных чисел
-print("Insert number: ")
-n = int(input())
-a = 1
-for number in range(2, n + 1, 2):
-    a = a * number
-print(a)
-#  END
+def test_is_prime():
+    assert is_prime(1) is False
+    assert is_prime(2) is True
+    assert is_prime(24) is False
+    assert is_prime(5) is True
+    assert is_prime(0) is False
+    assert is_prime(107) is True
+    assert is_prime(19) is True
+    print("Great your solution works!")
+test_is_prime()
 
-# BEGIN Task 6  Количество "а"
-string=input("Input sentence: ")
-s=0
-for n in string:
-    if n == "a":
-        s=s+1
-print(s)
-#  END
 
-# BEGIN Task 7   Количество гласных
-string=input("Input sentence: ")
-a=e=i=o=u=y=0
-for n in string:
-    if n == "a":
-        a+=1
-    if n == "e":
-        e += 1
-    if n == "i":
-        i += 1
-    if n == "o":
-        o += 1
-    if n == "u":
-        u += 1
-    if n == "y":
-        y += 1
-print(str(a+e+i+o+u+y) + f" (a-{a}, e-{e}, i-{i}, o-{o}, u-{u}, y-{y})")
-# END
+# TASK 5
+def merge(a, b):
+    n = m = 0
+    list = []
+    while n < len(a) and m < len(b):
+        if a[n] < b[m]:
+            list.append(a[n])
+            n += 1
+        else:
+            list.append(b[m])
+            m += 1
+    list += a[n:]
+    list += b[m:]
+    return list
 
-# BEGIN Task 7   Количество согласных
-string=input("Input sentence: ")
-let=0
-for n in string:
-    if n != "a" and n != "e" and n != "i" and n != "o" and n != "u" and n != "y" and n != " ":
-        let+=1
-print(let)
-# END
+def test_merge():
+    assert merge([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert merge([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+    assert merge([4, 5, 6], [1, 2, 3]) == [1, 2, 3, 4, 5, 6]
+    assert merge([2, 4, 6], [1, 3, 5]) == [1, 2, 3, 4, 5, 6]
+    assert merge([4, 5, 6], []) == [4, 5, 6]
+    assert merge([], [1, 3, 5]) == [1, 3, 5]
+    assert merge([], []) == []
+    print("Great your solution works!")
+test_merge()
 
-# BEGIN Task 8   Обратный порядок
-string = input("Input sentence: ")
-result = ""
-for current in range(len(string) - 1, -1, -1):
-    result += string[current]
-print(result)
-# END
+# TASK 6
+def has_substring(s, t):
+    search_len = len(t)
+    for index in range(0, len(s) - search_len + 1):
+        ser = s[index:index + search_len]
+        if ser == t:
+            return True
+    return False
 
-# BEGIN Task 9   Пирамида
-n = int(input("Input number: "))
-for number in range (1, n+1):
-    t=""
-    for inner in range(1, number + 1):
-        t+=" "+str(number)
-    print(t)
-# END
+def test_has_substring():
+    assert has_substring("some text", "text") is True
+    assert has_substring("some text", "some") is True
+    assert has_substring("", "text") is False
+    assert has_substring("text", "") is True  # любая строка содержит в себе пустую подстроку
+    assert has_substring("", "") is True  # даже пустая строка содержит в себе пустую подстроку :)
+    print("Great your solution works!")
+test_has_substring()
